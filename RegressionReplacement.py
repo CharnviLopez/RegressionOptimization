af57702e1cf9db03a03388006b583022c1ea0b40
Alejandro Lopez <147281834+CharnviLopez@users.noreply.github.com>
2023-10-11 10:35:19 -0500

    Updated comments in RegressionReplacement.py

--- RegressionReplacement.py
+++ RegressionReplacement.py
@@ -1,44 +1,40 @@
+#Project for Principle of Operation Research.
+#By Bradford Howe and Alejandro Lopez.
 
-# In response to Homework 3 Question 1 Part 1, this is a solution for Homework 1 Question 4.
-
+# In this code we want to solve regression using Linear Optimization.
+#We are using Gurobi for the Linear Optimization.
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
 import scipy.sparse as sp
 
 try:
-
-    # This names the model after its task- Checking my work for problem 1 on homework 4.
+    # The name is RegressionGPModel to based on the projects goal.
     RegressionGPModel  = gp.Model("RegressionReplacement")
 
-    # This is setting the coefficients for two continuous variables to be summed and maximized
-    # within the constraints set below.
+    # This are the two varaible for the Linear Optimization.
+    #b_0 is the intercetion of the regression line.
+    #b_1 is the slope for the regression line
     b_0 = RegressionGPModel.addVar(vtype = "C", name="b_0")
     b_1 = RegressionGPModel.addVar(vtype = "C", name="b_1")
     
-
     # Setting constrainst for the model.
-    #######################################
-    # Joy cannot work more than 240 minutes and spends 5 minutes on each toy.
-    #m.addConstr(5*C + 5*S + 5*T <= 240, "c1")
-    # Kim cannot work more than 360 minutes and spends 4, 8, and 6 minutes on
-    # cars, soldiers, and trains, respectively.
-    #RegressionGPModel.addConstr(b_0 <= 0, "c1")
-    #RegressionGPModel.addConstr(b_1 >= 0, "c2")
+    # Replace the first 5 with y and last 5 replace with x.
+    # The lowest we can go is zero error.
     RegressionGPModel.addConstr(5- b_0 + b_1*5 >= 0, "c3")
+    
     # Set objective function.
-   #for i in dataset
+    # Same as contrain replace the 5
+    # We want to least of error in the regression
     RegressionGPModel.setObjective( 5- b_0 + (b_1*5) , GRB.MINIMIZE)
 
     # This function runs the optimization.
     RegressionGPModel.optimize()
-    
     print("\n","\n","This is a solution for regression replacement.")
 
     # This loop prints the values for each decision variable in the model.
     for v in RegressionGPModel.getVars():
         print('%s %g' % (v.Varname, v.x))\
-        #print(b_0)
 
     # This prints the final value of the objective function.
     print('Obj: %g' % RegressionGPModel.ObjVal)

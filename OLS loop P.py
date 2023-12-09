e013a7c8058f42a2890f2b0e95fa1af10fac856a
Alejandro Lopez <147281834+CharnviLopez@users.noreply.github.com>
2023-12-09 17:32:44 -0600

    Update OLS loop P.py

--- OLS loop P.py
+++ OLS loop P.py
@@ -9,10 +9,10 @@ import time
 #url = 'https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDY6OOBVL2YHUB3H7LOZLU2EPQ'
 #Data = pd.read_csv(url)
 #Student 1 computer
-#Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
+Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
 #Data = pd.read_csv("C:/Users/BlueSteel/Desktop/R files/GurobiRegression/BFIsubset.csv")
 #Student 2 computer
-Data = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
+#Data = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
 
 #X = Data.iloc[0:99,0].values.reshape(-1, 1)
 #Y = Data.iloc[0:99,1]

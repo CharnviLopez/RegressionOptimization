42f41280789b0358daee9936b2b56c556934c9ec
Alejandro Lopez <147281834+CharnviLopez@users.noreply.github.com>
2023-12-09 16:52:02 -0600

    LAD single G.py

--- LAD single G.py
+++ LAD single G.py
@@ -7,17 +7,29 @@ import time
 #LAD single Gurobi
 
 try:
-    #Get a dataset
-    #Data = pd.read_csv('https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDZIN4H7PNL74J6XCAKZLHM53Q')
-    Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
+    #Get a dataset from XYregData.csv in Github
+    #url = 'https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDY6OOBVL2YHUB3H7LOZLU2EPQ'
+    #Data = pd.read_csv(url)
+    #Student 1 computer
+    #Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
+    #Data = pd.read_csv("C:/Users/BlueSteel/Desktop/R files/GurobiRegression/BFIsubset.csv")
+    #Student 2 computer
+    Data = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
+    
+    #Data range input
     X = Data.iloc[0:99,0]
     Y = Data.iloc[0:99,1]
 
+    #Timer start
     start = time.time()
-    # This names the model after its task.
+    
+    # Assign name for Gurobi
     RegressionGPModel  = gp.Model("RegressionReplacement")
     
-    # These are the intercept and slope in a regression.  
+    #Silence Gurobi Code
+    RegressionGPModel.setParam('OutputFlag', 0)
+    
+    # These are the intercept(b_0) and coefficient(b_1) in a regression.    
     b_0 = RegressionGPModel.addVar(vtype = "C", lb = -GRB.INFINITY, name="b_0")
     b_1 = RegressionGPModel.addVar(vtype = "C", lb = -GRB.INFINITY, name="b_1")
     
@@ -26,28 +38,26 @@ try:
     z_1 = RegressionGPModel.addVars( range(len(X)), vtype = "C", name = "z_1")
     z_2 = RegressionGPModel.addVars( range(len(X)), vtype = "C", name = "z_2")
     
-    # This allows for a Least Absolute Distance (LAD) regression method.
+    # This allows for a LAD regression method.
     RegressionGPModel.addConstrs(z[i] == (Y[i] - b_1*X[i] - b_0) for i in range(len(X)))
     RegressionGPModel.addConstrs(z[i] == z_1[i] - z_2[i] for i in range(len(X)))
     
+    # This allows for a LAD regression method.
     RegressionGPModel.setObjective(quicksum(z_1[i] + z_2[i] for i in range(len(X))), GRB.MINIMIZE)
-    
- 
     RegressionGPModel.optimize()
+    
+    #Run time, 
+        #First two lines is the read time by the code 
+        #The last one is read time by Gurobi
     end = time.time()
     SysMeasureRuntime = end - start
     GurMeasureRuntime = RegressionGPModel.Runtime
 
-    
-    print("\nGurobi coefficients and error for LAD immitation.")
-
-    # This loop prints the values for each decision variable in the model.
-    # for v in RegressionGPModel.getVars():
-    #     print( v.Varname, v.x)
-
+    #Print Gurobi data
     v = RegressionGPModel.getVars()
-    print(v[0])
-    print(v[1])
+    print("Gurobi Intercept:", b_0.X)
+    print("Gurobi Coefficient:", b_1.X)
+    
     print('SSerror:', RegressionGPModel.ObjVal)
     print("Gurobi measured run time for Gurobi: %f" % GurMeasureRuntime)
     print("System measured run time for Gurobi: ", SysMeasureRuntime)

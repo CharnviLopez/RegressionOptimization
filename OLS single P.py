7e4be401eb517190178c6d5f219cd76992411c70
Alejandro Lopez <147281834+CharnviLopez@users.noreply.github.com>
2023-12-09 17:48:37 -0600

    OLS single P.py

--- OLS single P.py
+++ OLS single P.py
@@ -3,23 +3,33 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import time
 
-#OLS single Python
+#Ordinary Least Squares (OLS) single with Python
 
-#Get a dataset
-#Data = pd.read_csv('https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDZQMHVQXNCHH2PUAP4ZLHMRIQ
-RegData = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
-#RegData = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
+#Get a dataset from XYregData.csv in Github
+#url = 'https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDY6OOBVL2YHUB3H7LOZLU2EPQ'
+#Data = pd.read_csv(url)
+#Student 1 computer
+#Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
+#Data = pd.read_csv("C:/Users/BlueSteel/Desktop/R files/GurobiRegression/BFIsubset.csv")
+#Student 2 computer
+Data = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
 
-RegStart = time.time() 
-Xreg = RegData.iloc[0:99,0].values.reshape(-1, 1)
-Yreg = RegData.iloc[0:99,1]
+#Timer start
+Start = time.time() 
+
+#Data range input
+X = Data.iloc[0:99,0].values.reshape(-1, 1)
+Y = Data.iloc[0:99,1]
+
+# OLS regression with sklearn package
 reg = LinearRegression().fit(Xreg,Yreg)
-RegEnd = time.time()
-#print("Intercept:", reg.intercept_)
-#print("Coefficients:", reg.coef_)
 
+#Timer stop
+End = time.time()
 
+#Print Python data
 print("\nPython OLS regression with sklearn package.")
-print("Total time", RegEnd - RegStart)        
-    
+print("Python Intercept:", reg.intercept_)
+print("Python Coefficients:", reg.coef_)
+print("Total time", End - Start)       
     

ac8ee1a6c05e197bdd71542ad84096fe9663e904
Alejandro Lopez <147281834+CharnviLopez@users.noreply.github.com>
2023-12-09 17:42:20 -0600

    OLS single G.py

--- OLS single G.py
+++ OLS single G.py
@@ -4,39 +4,54 @@ import pandas as pd
 import math
 import time
 
-#OLS single Gurobi
+#Ordinary Least Squares (OLS) single time with Gurobi
 
 try:
-    #Get a dataset
-    #Data = pd.read_csv('https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDZQ76XBFBUODUMMCRKZLHMNXA')
-    Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
+    #Get a dataset from XYregData.csv in Github
+    #url = 'https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDY6OOBVL2YHUB3H7LOZLU2EPQ'
+    #Data = pd.read_csv(url)
+    #Student 1 computer
+    #Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
+    #Data = pd.read_csv("C:/Users/BlueSteel/Desktop/R files/GurobiRegression/BFIsubset.csv")
+    #Student 2 computer
+    Data = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
+    
+     #Data range input
     X = Data.iloc[0:99,0]
     Y = Data.iloc[0:99,1]
 
+    # Assign name for Gurobi
     RegressionGPModel  = gp.Model("RegressionReplacement")
+    
+     #Silence Gurobi Code
+    RegressionGPModel.setParam('OutputFlag', 0)
+    
     # This sets the model to handle squaring the error equation.
     RegressionGPModel.Params.NonConvex = 2
-    # These are the intercept and slope in a regression.  
+    
+    # These are the intercept(b_0) and coefficient(b_1) in a regression.
     b_0 = RegressionGPModel.addVar(vtype = "C", lb = -GRB.INFINITY, name="b_0")
     b_1 = RegressionGPModel.addVar(vtype = "C", lb = -GRB.INFINITY, name="b_1")
     
-    # This objective equariont squares the error to immitate an Ordinary Least Squares (OLS) regression.
+    #Timer start
     start = time.time()
+    
+    #Gurobi runs the OLS regression
     RegressionGPModel.setObjective(quicksum( (Y[i] - b_1*X[i] - b_0)*(Y[i] - b_1*X[i] - b_0)for i in range(len(X))), GRB.MINIMIZE)
-
     RegressionGPModel.optimize()
+    
+    #Run time, 
+        #First two lines is to stop the timer
+        #The last one is read time by Gurobi
     end = time.time()
     SysMeasureRuntime = end - start
     GurMeasureRuntime = RegressionGPModel.Runtime
-
-    print("\nGurobi coefficients and error for OLS immitation.")
-
-    # This loop prints the values for each decision variable in the model.
-    for v in RegressionGPModel.getVars():
-        print( v.Varname, v.x)
-
-    # This prints the final value of the objective function.
-    print('SSerror:', RegressionGPModel.ObjVal)
+    
+    #Print Gurobi data
+    v = RegressionGPModel.getVars()
+    print("Gurobi Intercept:", b_0.X)
+    print("Gurobi Coefficient:", b_1.X)
+    
     print("Gurobi measured run time for Gurobi: %f" % GurMeasureRuntime)
     print("System measured run time for Gurobi: ", SysMeasureRuntime)
 

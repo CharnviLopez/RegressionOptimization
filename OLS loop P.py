b34a7218e10fa1eb09440aada242808f83b88e0d
Alejandro Lopez <147281834+CharnviLopez@users.noreply.github.com>
2023-12-09 17:21:58 -0600

    OLS loop P.py

--- OLS loop P.py
+++ OLS loop P.py
@@ -3,22 +3,34 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import time
 
-#OLS single Python 
+#Ordinary Least Squares (OLS) loop time with Python 
 
-#Get a dataset
-#Data = pd.read_csv('https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDZQMHVQXNCHH2PUAP4ZLHMRIQ
-RegData = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
-#RegData = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
+#Get a dataset from XYregData.csv in Github
+#url = 'https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDY6OOBVL2YHUB3H7LOZLU2EPQ'
+#Data = pd.read_csv(url)
+#Student 1 computer
+#Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
+#Data = pd.read_csv("C:/Users/BlueSteel/Desktop/R files/GurobiRegression/BFIsubset.csv")
+#Student 2 computer
+Data = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
 
-RegStart = time.time() 
-Xreg = RegData.iloc[0:99,0].values.reshape(-1, 1)
-Yreg = RegData.iloc[0:99,1]
-reg = LinearRegression().fit(Xreg,Yreg)
-RegEnd = time.time()
+#X = Data.iloc[0:99,0].values.reshape(-1, 1)
+#Y = Data.iloc[0:99,1]
 
-#print("Coefficients:", reg.coef_)
-#print("Intercept:", reg.intercept_)
-
-print("\nPython OLS regression with sklearn package.")
-print("Total time", RegEnd - RegStart)        
+#Timer start
+Start = time.time() 
+#Data range input, and start of loop
+for i in range(3,99):
+    X = Data.iloc[0:i, 0].values.reshape(-1, 1)
+    Y = Data.iloc[0:i, 1]
     
+    # OLS regression with sklearn package
+    reg = LinearRegression().fit(X,Y)
+    
+    #Print i to know which regression it's on
+    print(i)
+    
+#Time stop and print results
+End = time.time()
+print("\nPython OLS regression with sklearn package.")
+print("Total time", End - Start)  

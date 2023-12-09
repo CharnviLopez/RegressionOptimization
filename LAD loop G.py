175c213ada315de3227031b42af8ddea546562f8
Alejandro Lopez <147281834+CharnviLopez@users.noreply.github.com>
2023-12-09 15:54:00 -0600

    LAD loop G.py
    v2

--- LAD loop G.py
+++ LAD loop G.py
@@ -4,22 +4,30 @@ import pandas as pd
 import math
 import time
 
-#LAD loop Gurobi
+#LAD loop time in Gurobi
 
 try:
-    #Get a dataset
-    #Data = pd.read_csv('https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDZBR2M6XQJWBZLDUUKZLHNFPA')
+    #Get a dataset from XYregData.csv in Github
+    #url = 'https://raw.githubusercontent.com/CharnviLopez/RegressionOptimization/main/XYregData.csv?token=GHSAT0AAAAAACK5BZDY6OOBVL2YHUB3H7LOZLU2EPQ'
+    #Data = pd.read_csv(url)
+    #Student 1 computer
     Data = pd.read_csv("C:/RegressionOptimizationFoyer/RegressionOptimization/XYregData.csv")
-
-    # This names the model after its task.
+    #Student 2 computer
+    #Data = pd.read_csv("/Users/Crow/Desktop/POR/XYregData.csv")
+    
+    # Assign name to Gurobi
     RegressionGPModel  = gp.Model("RegressionReplacement")
+    
+    #Silence Gurobi Code
     RegressionGPModel.setParam('OutputFlag', 0)
     
-    # These are the intercept and slope in a regression.  
+    # These are the intercept(b_0) and coefficient(b_1) in a regression.  
     b_0 = RegressionGPModel.addVar(vtype = "C", lb = -GRB.INFINITY, name="b_0")
     b_1 = RegressionGPModel.addVar(vtype = "C", lb = -GRB.INFINITY, name="b_1")
     
+    #Time
     start = time.time()
+    #Data range input
     for i in range(99):
         X = Data.iloc[0:i,0]
         Y = Data.iloc[0:i,1]
@@ -32,12 +40,17 @@ try:
         # This allows for a Least Absolute Distance (LAD) regression method.
         RegressionGPModel.addConstrs(z[i] == (Y[i] - b_1*X[i] - b_0) for i in range(len(X)))
         RegressionGPModel.addConstrs(z[i] == z_1[i] - z_2[i] for i in range(len(X)))
-    
+        
+        #Gurobi runs the LAD regression
         RegressionGPModel.setObjective(quicksum(z_1[i] + z_2[i] for i in range(len(X))), GRB.MINIMIZE)
         RegressionGPModel.optimize()
+        
+        #Print i to know in which regression it on
         print(i)
-        # for v in RegressionGPModel.getVars():
-        #     print( v.Varname, v.x)
+        
+    #Run time, 
+        #First two lines is the read time by the code 
+        #The last one is read time by Gurobi
     end = time.time()
     SysMeasureRuntime = end - start
     GurMeasureRuntime = RegressionGPModel.Runtime
